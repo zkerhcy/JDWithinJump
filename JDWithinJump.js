42d7ed62cfd40b9d4c75b5d7612bf63991e06e73
chenzhao <czhao5956@gmail.com>
2018-08-22 14:59:26 +0800

    :bug: Exclude login page.

--- JDWithinJump.js
+++ JDWithinJump.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         京东页内跳转
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.3
 // @description  京东在非商品详情页面打开超链接时在当前窗口显示,不打开新窗口，减少购物时打开的页面数量
 // @author       Zkerhcy
 // @include      *//*.jd.com/*
+// @exclude      *//passport.jd.com/*
 // @require      https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/zkerhcy/JDWithinJump/master/JDWithinJump.js
 // @downloadURL  https://raw.githubusercontent.com/zkerhcy/JDWithinJump/master/JDWithinJump.js
