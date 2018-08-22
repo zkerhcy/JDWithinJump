7323543edfb753db5e8dd62d95a9b51512773634
chenzhao <czhao5956@gmail.com>
2018-08-22 15:02:15 +0800

    :bug: Fix version.

--- JDWithinJump.js
+++ JDWithinJump.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         京东页内跳转
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.5
 // @description  京东在非商品详情页面打开超链接时在当前窗口显示,不打开新窗口，减少购物时打开的页面数量
 // @author       Zkerhcy
 // @include      *//*.jd.com/*
