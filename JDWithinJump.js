787ad16270f1b90d14c90a3b1678acee0afaef87
chenzhao <czhao5956@gmail.com>
2018-08-30 08:50:48 +0800

    :bug: 修复购物车中商品删除、移到关注等按钮无法点击的问题。

--- JDWithinJump.js
+++ JDWithinJump.js
@@ -1,27 +1,24 @@
 // ==UserScript==
 // @name         京东页内跳转
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  京东在非商品详情页面打开超链接时在当前窗口显示,不打开新窗口，减少购物时打开的页面数量
 // @author       Zkerhcy
 // @include      *//*.jd.com/*
 // @exclude      *//passport.jd.com/*
-// @require      https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/zkerhcy/JDWithinJump/master/JDWithinJump.js
 // @downloadURL  https://raw.githubusercontent.com/zkerhcy/JDWithinJump/master/JDWithinJump.js
 // @grant        none
 // ==/UserScript==
 
-;
-(function () {
+;(function() {
   'use strict'
-  $(function () {
+  $(function() {
     var url = window.location.host
     if (url.indexOf('item.jd.com') === -1) {
-      // $('.nickname').removeAttr('target');
-      $('a').each(function (index, element) {
+      $('a').each(function(index, element) {
         $(this).removeAttr('target')
-      });
+      })
     }
   })
 })()
